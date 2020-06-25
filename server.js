54e39a939bfc05e7edc2f5568c0a1cfc21791a0a
JasmineKhalimova <jasmine.khalimova@yahoo.com>
2020-06-25 14:14:36 +0100

    added new packages

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
  
 app.use(compression());
+require("dotenv").config();
 app.use(express.static(path.join(__dirname, 'build')));
  
 app.get('*', function(req, res) {
